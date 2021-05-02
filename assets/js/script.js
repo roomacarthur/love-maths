8827a35c2e4ddd8cff08c37f02a65e3a37d02a4e
Roomacarthur <roomacarthur@gmail.com>
2021-05-02 14:05:09 +0100

    JS for addition selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,21 +10,28 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`)
+                runGame(gameType);
             }
         })
     }
+    runGame("addition");
 })
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer ghas been processed.
  */
-function runGame() {
+function runGame(gameType) {
     //create two random numbers for questions.
     let num1 = Math.floor(Math.random()* 25) + 1;
     let num2 = Math.floor(Math.random()* 25) + 1;
-    
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Uknown game type: ${gameType}`);
+        throw `Uknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -43,11 +50,15 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
 
 }
 
