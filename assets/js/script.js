29c8fadbe3aecae6c80bd354bb7af2208b2f6953
Roomacarthur <roomacarthur@gmail.com>
2021-05-02 18:30:52 +0100

    updated division function

--- assets/js/script.js
+++ assets/js/script.js
@@ -133,8 +133,9 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+
+    document.getElementById("operand1").textContent = operand1 * operand2;
+    document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "/";
 
 }
