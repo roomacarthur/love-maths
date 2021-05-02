160983e4949ee52da1012078d1a60f659add35bd
Roomacarthur <roomacarthur@gmail.com>
2021-05-02 13:12:17 +0100

    Added event listeners for game buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+//EVENT LISTENERS
+//Alow DOM to load before running game.
+//Retrieve buttons and add event listeners
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`)
+            }
+        })
+    }
+})
+
+
 function runGame() {
 
 }
